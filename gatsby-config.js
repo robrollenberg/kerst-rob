484f6aba5a4a65cc3cf6b3338989795b81048471
Rob Rollenberg <rob@jepr.nl>
2020-12-22 17:04:11 +0100

    Removed manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,17 +15,6 @@ module.exports = {
         },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`, // This path is relative to the root of the site.
-            },
-        },
         `gatsby-plugin-emotion`,
         `gatsby-plugin-layout`,
         // this (optional) plugin enables Progressive Web App + Offline functionality
